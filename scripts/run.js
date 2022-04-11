574840cd27772617df6bd49a3826f9b4f0aaf113
aimachieve <aim1005achieve@gmail.com>
2022-04-11 12:15:41 +0500

    ...

---
+++ scripts/run.js
@@ -0,0 +1,31 @@
+
+require("dotenv").config();
+
+async function main() {
+
+  // Get owner/deployer's wallet address
+  const [owner] = await hre.ethers.getSigners();
+
+  // Get contract that we want to deploy
+  const contractFactory = await hre.ethers.getContractFactory("ParaPreppers");
+  console.log("cid:", process.env.PRIVATE_KEY);
+
+  // Deploy contract with the correct constructor arguments
+  const contract = await contractFactory.deploy();
+
+  // Wait for this transaction to be mined
+  await contract.deployed();
+
+  // Get contract address
+  console.log("Contract deployed to:", contract.address);
+
+  // Get all token IDs of the owner
+
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
